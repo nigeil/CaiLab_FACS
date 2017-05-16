a08a99fa400a3ec23e5b1e2bdd57e7d81d73f835
Nigel Michki <nigeil@yahoo.com>
2017-05-16 14:57:27 -0400

    Changes related to making microcontroller testing more robust and amenable to the user.

---
+++ testing/microcontroller_timing_test.py
@@ -0,0 +1,44 @@
+#!/bin/python
+# Tests how fast the microcontroller can read all channels and return
+# to the top of the loop.
+
+import serial
+import time
+import re
+import numpy as np
+import io
+
+t_list = []
+voltages_list = []
+i = 0
+n_itr = 10000
+
+#mc = serial.Serial('/dev/ttyACM0', timeout=0.001, rtscts=True)
+mc = serial.Serial('/dev/ttyACM0', 250000, timeout=10.0, rtscts=True)
+sio = io.TextIOWrapper(io.BufferedRWPair(mc, mc))
+
+while (i < n_itr):
+    dat = sio.readline()
+    if dat is not '':
+        t = re.findall(r'TIME: (.*?) RUNSTATE:',dat)
+        if (t not in[[], None]):
+            t = int(t[0])
+            t_list.append(t)
+            i += 1
+        voltages = re.findall(r'CURR_VOLTAGES: (.*?) THRESH_VOLTAGES:', dat)
+        if (voltages not in [[], None]):
+            voltages = np.fromstring(voltages[0], sep=' ')
+            voltages_list.append(voltages)
+            i += 1
+
+t_list = np.array(t_list)
+t_mean = np.mean(t_list)
+t_stddev = np.std(t_list)
+
+voltages_list = np.array(voltages_list)
+voltages_mean = np.mean(voltages_list, axis=0)
+voltages_stddev = np.std(voltages_list, axis=0)
+
+print("avg. time +/- stddev: " + str(t_mean) + " +/- " + str(t_stddev) + "us")
+print("avg .voltages +/- stddev: " + str(voltages_mean) + " +/- " + str(voltages_stddev) + " mV")
+
